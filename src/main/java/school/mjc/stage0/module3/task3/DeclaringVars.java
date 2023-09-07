e5c686c1dc77e1a06568d49522ba1653c88a636c
Krzysztof <krzys.zolnowski@gmail.com>
2023-09-07 10:31:14 +0200

    Module 3 task 3 repaired second task

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -3,7 +3,9 @@ package school.mjc.stage0.module3.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         int a, b, c;
-        a = 10; b = 10; c = 10;
+        a = 10;
+        b = 10;
+        c = 10;
         System.out.println(c);
     }
 }
